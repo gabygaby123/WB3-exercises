8d5e0bb5bdb3ce52ab115dfcba00a33535751ee3
gabygaby123 <108373391+gabygaby123@users.noreply.github.com>
2023-10-24 15:38:13 -0700

    commit

---
+++ functions/more_functions.js
@@ -0,0 +1,62 @@
+// function displayMailingLabel(name,address,city,state,zip){
+//     console.log("Name: " + name);
+//     console.log("Address: " + address );
+//     console.log("City: " + city);
+//     console.log("State: " + state);
+//     console.log("Zip: " + zip);
+// }
+
+// First Function
+function displayMailingLabel(name,address,city,state,zip){
+    console.log(" Name: " + name);
+    console.log(" Address: " + address);
+    console.log(" City: " + city);
+    console.log(" State: " + state);
+    console.log(" Zip: " + zip);
+}
+
+let someName = "Gaby"
+let someAddress = "123 st."
+let someCity = "Phoenix"
+let someState = "AZ"
+let someZip = "85009"
+displayMailingLabel(someName,someAddress,someCity,someState,someZip)
+console.log()
+someName = "Gabe"
+someAddress = "321 st."
+someCity = "Tempe"
+someState = "AZ"
+someZip = "85281"
+displayMailingLabel(someName,someAddress,someCity,someState,someZip)
+console.log()
+
+//Second Function
+function addNumbers (num1, num2){
+    sum = num1 + num2
+    console.log(num1 + "+" + num2 + "=" + sum);
+}
+let someNum1 = 5;
+let someNum2 = 10;
+addNumbers(someNum1,someNum2);
+
+someNum1 = 15;
+someNum2 = 20;
+addNumbers(someNum1,someNum2);
+
+//Third Function
+function displayReceipt(totalDue, amountPaid){
+    console.log("Total Due: $" + totalDue)
+    console.log("Amount Paid: $" + amountPaid)
+    console.log("Change Due: $" + (amountPaid - totalDue))
+}
+
+let total = 20;
+let paidAmount = 25;
+displayReceipt(total,paidAmount)
+total = 20;
+paidAmount = 20;
+displayReceipt(total,paidAmount)
+total = 25;
+paidAmount = 20;
+displayReceipt(total,paidAmount)
+
---
+++ functions/simple_functions.js
@@ -0,0 +1,16 @@
+function favoriteThings(){
+    console.log("Gabriela")
+    console.log("Shrek") 
+    console.log("Junior H") 
+}
+function whyImHere(){
+    console.log("I joined Learn To Code to learn to code")
+}
+
+function favoritePlace(){
+    console.log("I like to visit California because of the beach")
+}
+
+favoriteThings();
+whyImHere();
+favoritePlace();
